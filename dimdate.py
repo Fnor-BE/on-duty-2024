32c4e2d38ea5323303a912fe86657ebbd63693c4
Fnor <166872478+Fnor-BE@users.noreply.github.com>
2025-01-22 13:53:07 +0100

    Adds dimdate and holidays management

---
+++ dimdate.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+def create_dimdate(start: str, end:str) -> pd.DataFrame:
+    dimdate = pd.DataFrame({
+        'Date': pd.date_range(start=start, end=end, freq="D")
+    })
+    
+    dimdate['DayOfWeek'] = dimdate['Date'].dt.weekday + 1
+    dimdate['DayOfWeekText'] = dimdate['Date'].dt.day_name()
+    dimdate['DayOfWeekShort'] = dimdate['Date'].dt.strftime('%a')
+    
+    dimdate['IsWeekend'] = np.where( dimdate['DayOfWeek'] > 5, 1, 0 )
+    
+    return dimdate
+
+def add_holidays(dimdate: pd.DataFrame, holidays:list) -> pd.DataFrame:
+    
+    dimdate['IsHoliday'] = 0
+    dimdate['DaysToHoliday'] = 365
+    
+    for day in holidays:
+        dimdate.loc[dimdate['Date'] == day, 'IsHoliday'] = 1
+        
+    
+    dimdate['DaysToHoliday'] = dimdate['Date'].apply(
+        lambda date: min(abs((date - pd.to_datetime(holiday)).days) for holiday in holidays)
+    )
+    
+    return dimdate
+
+
+
+if __name__ == '__main__':
+    print(create_dimdate('2024-01-01', '2024-12-31'))
